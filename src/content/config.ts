e5a356dd5845e85277b76f067dc800cc069d2d6d
Saddam Azad <saddam.azad@me.com>
2025-03-04 09:06:31 +0600

    blog

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,3 +1,4 @@
+import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
 const docs = defineCollection({
@@ -22,7 +23,22 @@ const legal = defineCollection({
   })
 });
 
-export const collections = { docs, legal }
+const blog = defineCollection({
+	// Load Markdown and MDX files in the `src/content/blog/` directory.
+	// loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+	}),
+});
+
+
+export const collections = { docs, legal, blog }
 
 export type Sidebar = Record<string, { text: string; link: string }[]>;
 
